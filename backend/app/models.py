2c2e85f422ce49032fd20a4d94949ebcd6652176
Gustavo Carvalho <99373133+gustavosett@users.noreply.github.com>
2024-11-16 02:16:25 -0300

    feat: new models operations

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,20 @@
-from datetime import datetime
+from datetime import datetime, time
+from enum import Enum
+from typing import Optional
 import uuid
 
 from pydantic import EmailStr
 from sqlmodel import Field, Relationship, SQLModel
 from pydantic_br import CPFDigits
 
+class WeekEnum(str, Enum):
+    Monday = "monday"
+    Tuesday = "tuesday"
+    Wednesday = "wednesday"
+    Thursday = "thursday"
+    Friday = "friday"
+    Saturday = "saturday"
+    Sunday = "sunday"
 
 # Shared properties
 class UserBase(SQLModel):
@@ -51,6 +61,7 @@ class User(UserBase, table=True):
     items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
     restaurants: list["Restaurant"] = Relationship(back_populates="owner", cascade_delete=True)
     books: list["Book"] = Relationship(back_populates="user", cascade_delete=True)
+    payments: list["Payment"] = Relationship(back_populates="user", cascade_delete=True)
 
 
 # Properties to return via API, id is always required
@@ -60,6 +71,7 @@ class UserPublic(UserBase):
 class UserMe(UserPublic):
     restaurants: list["RestaurantPublic"]
     books: list["BookPublic"]
+    payments: list["PaymentPublic"]
 
 
 class UsersPublic(SQLModel):
@@ -72,7 +84,6 @@ class RestaurantBase(SQLModel):
     address: str | None = Field(default=None, max_length=255)
     phone: str | None = Field(default=None, max_length=255)
     image: str | None = Field(default=None)
-    rating: float = 5.0
     book_price: int = 0
 
 class RestaurantCreate(RestaurantBase):
@@ -83,25 +94,49 @@ class RestaurantUpdate(RestaurantBase):
     
 class Restaurant(RestaurantBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    rating: float = 5.0
     owner_id: uuid.UUID = Field(
         foreign_key="user.id", nullable=False, ondelete="CASCADE"
     )
     owner: User | None = Relationship(back_populates="restaurants")
     items: list["Item"] = Relationship(back_populates="restaurant", cascade_delete=True)
     books: list["Book"] = Relationship(back_populates="restaurant", cascade_delete=True)
+    operating_date_times: list["OperatingDateTime"] = Relationship(back_populates="restaurant", cascade_delete=True)
 
 class RestaurantPublic(RestaurantBase):
     id: uuid.UUID
     owner_id: uuid.UUID
+    rating: float = 5.0
 
 class RestaurantFull(RestaurantPublic):
     items: list["ItemPublic"]
     books: list["BookPublic"] # yeah i know it'll show all user's books
+    operating_date_times: list["OperatingDateTime"]
 
 class RestaurantsPublic(SQLModel):
     data: list[RestaurantPublic]
     count: int
 
+class OperatingDateTimeBase(SQLModel):
+    restaurant_id: uuid.UUID
+    day_of_week: WeekEnum
+    open_time: time
+    close_time: time
+    
+class OperatingDateTimeCreate(SQLModel):
+    day_of_week: WeekEnum
+    open_time: time
+    close_time: time
+
+class OperatingDateTimeUpdate(OperatingDateTimeCreate):
+    pass
+
+class OperatingDateTime(OperatingDateTimeBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    restaurant_id: uuid.UUID = Field(
+        foreign_key="restaurant.id", nullable=False, ondelete="CASCADE"
+    )
+    restaurant: Restaurant | None = Relationship(back_populates="operating_date_times")
 
 # Shared properties
 class ItemBase(SQLModel):
@@ -169,6 +204,7 @@ class Book(BookBase, table=True):
     created_at: datetime = Field(default_factory=datetime.utcnow)
     user: User | None = Relationship(back_populates="books")
     restaurant: Restaurant | None = Relationship(back_populates="books")
+    payments: list["Payment"] = Relationship(back_populates="book", cascade_delete=True)
 
 class BookPublic(BookBase):
     id: uuid.UUID
@@ -178,6 +214,74 @@ class BookPublic(BookBase):
 
 class BooksPublic(SQLModel):
     data: list[BookPublic]
+    count: int
+    
+class PaymentBase(SQLModel):
+    book_id: uuid.UUID
+    owner_id: uuid.UUID
+    payment_type: str = Field(default="pix") # pix, credit_card, debit_card
+
+class PaymentCreate(PaymentBase):
+    pass
+
+class PaymentUpdate(PaymentBase):
+    status: str = Field(default="pending") # pending, paid, cancelled, failed
+
+class Payment(PaymentBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    value: int
+    status: str = Field(default="pending") # pending, paid, cancelled, failed
+    token: str | None = Field(default=None)
+    book_id: uuid.UUID = Field(
+        foreign_key="book.id", nullable=False
+    )
+    owner_id: uuid.UUID = Field(
+        foreign_key="user.id", nullable=False
+    )
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    book: Book | None = Relationship(back_populates="payments")
+    user: User | None = Relationship(back_populates="payments")
+
+class Calendario(SQLModel):
+    criacao: datetime
+    expiracao: int
+
+class Local(SQLModel):
+    id: Optional[int] = None
+    location: Optional[str] = None
+    tipoCob: Optional[str] = None
+
+class Devedor(SQLModel):
+    cnpj: Optional[str] = None
+    nome: Optional[str] = None
+
+class Valor(SQLModel):
+    original: Optional[str] = None
+
+class Charge(SQLModel):
+    txid: str
+    calendario: Optional[Calendario] = None
+    revisao: Optional[int] = None
+    loc: Optional[Local] = None
+    location: Optional[str] = None
+    status: Optional[str] = None
+    devedor: Optional[Devedor] = None
+    valor: Optional[Valor] = None
+    chave: Optional[str] = None
+    solicitacaoPagador: Optional[str] = None
+    pixCopiaECola: Optional[str] = None
+
+class PaymentPublic(PaymentBase):
+    id: uuid.UUID
+    value: int
+    status: str = Field(default="pending") # pending, paid, canceled
+    created_at: datetime
+    
+class PaymentCharge(PaymentPublic):
+    charge: Optional[Charge] = None
+
+class PaymentsPublic(SQLModel):
+    data: list[PaymentPublic]
     count: int
     
 # Generic message
